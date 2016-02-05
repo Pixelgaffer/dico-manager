bf143bc23f571eeb01926efdc1640947ea0f207a
nicosio2 <nico.gruendel@web.de>
2016-02-05 14:45:32 +0100

    initial commit

---
+++ src/main/java/com/cherryworm/dico/DicoManagerApplication.java
@@ -0,0 +1,12 @@
+package com.cherryworm.dico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cherryworm/dico/DicoManagerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cherryworm.dico;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DicoManagerApplication.class)
+@WebAppConfiguration
+public class DicoManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
